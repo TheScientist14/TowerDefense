65efe6649bd71f1996e8a7536e9ba8696ac13787
TheScientist14 <37724964+TheScientist14@users.noreply.github.com>
2021-10-07 13:21:34 +0200

    Button added

---
+++ Assets/AgentButtonBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AgentButtonBehaviour : MonoBehaviour
+{
+    public GameObject agent;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(SelectTurret);
+        cam = Camera.main;
+    }
+
+    void SelectTurret()
+    {
+        Debug.Log("Hello");
+        GameObject turret = Instantiate(agent, cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y)), Quaternion.identity);
+        while (!Input.GetMouseButton(0))
+        {
+
+        }
+    }
+}
