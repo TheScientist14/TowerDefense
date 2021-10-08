360ef0d9a53cbaa96f2952c602a4715bb177cca8
Anteckk <antoine.varniern@gmail.com>
2021-10-08 12:25:21 +0200

    Add HealthBar in the Enemy

--- Assets/Template/Scripts/ThiefBehaviour.cs
+++ Assets/Template/Scripts/ThiefBehaviour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class ThiefBehaviour : MonoBehaviour
 
@@ -11,6 +12,9 @@ public class ThiefBehaviour : MonoBehaviour
     private int health;
     private int healthMax;
     private int damage;
+    public Slider healthBar;
+    public Canvas canva;
+    private Camera camera;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +24,21 @@ public class ThiefBehaviour : MonoBehaviour
         helicopter = GameObject.Find("Helicopter");
         agent = GetComponent<NavMeshAgent>();
         agent.destination = helicopter.transform.position;
+        healthBar.maxValue = health;
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        /*Vector3 direction = transform.position - Camera.main.transform.position; // calcule la direction
+        Quaternion lookRotation = Quaternion.LookRotation(direction); // calcule la rotation
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * 50).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);*/
+        //Quaternion rotation = Quaternion.LookRotation(camera.transform.position);
+        //healthBar.transform.rotation = Quaternion.Euler(camera.transform.rotation.x, camera.transform.rotation.y, camera.transform.rotation.z);
+        canva.transform.LookAt(camera.transform);
         if ( health <= 0)
         {
             GameManagement.AddMoney(5);
@@ -36,7 +50,8 @@ public class ThiefBehaviour : MonoBehaviour
     public void GetDamage(int damage)
     {
         health -= damage;
-        Debug.Log("Hit health = " + health);
+
+        healthBar.value = health;
     }
 
     public int PointLose()
