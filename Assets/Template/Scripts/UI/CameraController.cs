64f23ab8173b21e2705add4d98a8cdb46dac9df7
TheScientist14 <saurytom@gmail.com>
2021-10-15 17:56:23 +0200

    Camera mouse drag, tried to fix nav mesh

--- Assets/Template/Scripts/UI/CameraController.cs
+++ Assets/Template/Scripts/UI/CameraController.cs
@@ -5,33 +5,74 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public float speed;
+    private float speed;
+    private Vector3 previousPos = Vector3.negativeInfinity;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 20f;
+        speed = 25f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Camera management
-        if (Input.GetKey(KeyCode.UpArrow) && transform.position.z >= -2)
+        Vector3 position = transform.position;
+
+        // moving camera with arrow keys
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += (Vector3.back * speed * Time.deltaTime);
+            position += (Vector3.back * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.DownArrow) && transform.position.z <= 58)
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += (Vector3.forward * speed * Time.deltaTime);
+            position += (Vector3.forward * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x >= -10)
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            position += (Vector3.left * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x < 10)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            position += (Vector3.right * speed * Time.deltaTime);
         }
+
+        // right click dragging camera
+        if (Input.GetMouseButtonDown(1))
+        {
+            previousPos = Input.mousePosition;
+        }
+        if (!previousPos.Equals(Vector3.negativeInfinity))
+        {
+            Vector3 currentPos = Input.mousePosition;
+            Vector3 delta = (currentPos - previousPos);
+            delta.z = delta.y;
+            delta.y = 0;
+            position += (delta * 0.05f);
+            previousPos = currentPos;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            previousPos = Vector3.negativeInfinity;
+        }
+
+        // limiting camera area
+        if (position.z < -2)
+        {
+            position.z = -2;
+        }
+        if (position.z > 58)
+        {
+            position.z = 58;
+        }
+        if (position.x < -10)
+        {
+            position.x = -10;
+        }
+        if (position.x > 10)
+        {
+            position.x = 10;
+        }
+        transform.position = position;
     }
 }
